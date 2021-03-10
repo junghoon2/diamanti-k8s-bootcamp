c8b1ce4cc48697be2ac69a69776cf869d7cb781e
jerry <erdia22@gmail.com>
2021-03-10 14:04:33 +0900

    add litmus

--- 72.DiaDemoPerftier/demo.sh
+++ 72.DiaDemoPerftier/demo.sh
@@ -42,24 +42,3 @@ dctl cluster status
 
 echo "See the Diamanti Admin Pages"
 
-
-# run "echo 'dctl network list' & dctl network list"
-# # run "dctl network create blue -s 172.16.225.0/24 --start 172.16.225.203 --end 172.16.225.254  -g 172.16.225.1 -v 225"
-# # run "dctl network list"
-
-# # run "dctl volume list"
-# # run "dctl volume create test-1 -s 10G"
-# # run "dctl volume list"
-# # run "dctl volume delete test-1 -y"
-
-# run "dctl perf-tier list"
-# # run "dctl perf-tier create low -i 1k -b 10M"
-# # run "dctl perf-tier list"
-
-# $(dirname ${BASH_SOURCE})/create-iperf-fio.sh dia01 dia02 dia03 blue
-# run "kubectl get pods -o wide"
-
-# run "kubectl get pods -o wide"
-
-# run "dctl cluster status"
-
