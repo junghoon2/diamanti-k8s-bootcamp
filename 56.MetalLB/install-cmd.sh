988a1dab385d5a7283df57f4fb451a7285a54666
jerry <erdia22@gmail.com>
2021-05-01 02:31:04 +0900

    add demo pjt

--- 56.MetalLB/install-cmd.sh
+++ 56.MetalLB/install-cmd.sh
@@ -1,5 +1,9 @@
 # 최신 버전으로 설치 할 것
 # kube-proxy CM 변경, ipvs to strictARP: true 변경 
+
+## kube-proxy CM 변경 후 rollout restart 한다.  
+kubectl rollout restart -n kube-system daemonset kube-proxy
+
 # Diamanti 해당 없음
 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
@@ -7,3 +11,4 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manife
 
 # On first install only
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
--- 56.MetalLB/kube-proxy-rollout.md
+++
@@ -1,2 +0,0 @@
-## kube-proxy CM 변경 후 rollout restart 한다.  
-kubectl rollout restart -n kube-system daemonset kube-proxy
