a11c973428e269425ec0e6276fa7bc57d9c83306
jerry <erdia22@gmail.com>
2021-05-25 01:45:24 +0900

    modify gitlab

--- 56.MetalLB/install-cmd.sh
+++ 56.MetalLB/install-cmd.sh
@@ -6,8 +6,8 @@ kubectl rollout restart -n kube-system daemonset kube-proxy
 
 # Diamanti 해당 없음
 
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
 
 # On first install only
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
